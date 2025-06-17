b05eeb97aa66598cee321cafed104a143e188d65
Hector Andres Castro <hectora12@hotmail.com>
2025-06-16 19:15:01 -0500

    commit

---
+++ src/Js/main.js
@@ -0,0 +1,11 @@
+
+let myWeight
+let myHeight
+let imc
+
+myWeight = prompt(message = "please, write your weight")
+myHeight = prompt(message = "please, write your height")
+
+imc = myWeight / (myHeight * myHeight)
+
+alert("your imc is ", imc)
